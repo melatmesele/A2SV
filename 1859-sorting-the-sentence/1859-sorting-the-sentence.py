d1a28c1bc50b817ef88201493fe4a92eb671f1e1
melatmesele <94954601+melatmesele@users.noreply.github.com>
2022-11-14 19:08:58 -0800

    Time: 59 ms (34.61%), Space: 14 MB (11.66%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,15 +1,30 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
+        # s = "is2 sentence4 This1 a3"
+        # d = "is2 " , "sentence4" ,  "This1"  , "a3"
+        # r = "" , "" ,"", ""
+        new=s.split()
+        ans =[0]*len(new)  
+        for word in new:
+         
+            ans[int(word[-1])-1] = word[:-1]
+        return " ".join(ans)
+    
+            
+                
         
-        # [1] split into words
-        spl = s.split(" ")
         
-        # [2] sort by last symbol (i.e., word number)
-        srt = sorted(spl, key=lambda w: w[-1])
+#         # [1] split into words
+#         spl = s.split(" ")
         
-        # [3] extract a word (i.e., remove word number)
-        wrd = [w[:-1] for w in srt]
+#         # [2] sort by last symbol (i.e., word number)
+#         srt = sorted(spl, key=lambda w: w[-1])
         
-        # [4] join words into a sentence
-        return " ".join(wrd)
+#         # [3] extract a word (i.e., remove word number)
+#         wrd = [w[:-1] for w in srt]
+        
+#         # [4] join words into a sentence
+#         return " ".join(wrd)
+
+    
         
