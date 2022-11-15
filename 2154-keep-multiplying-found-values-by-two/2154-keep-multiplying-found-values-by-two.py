9573510d7f48efbb91e95fbd9fb8ab04477304fa
melatmesele <94954601+melatmesele@users.noreply.github.com>
2022-11-15 05:28:42 -0800

    Time: 155 ms (18.04%), Space: 14 MB (88.60%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -1,11 +1,17 @@
 class Solution:
     def findFinalValue(self, nums: List[int], original: int) -> int:
+        # nums.sort()
+        # # [5,3,6,1,12]      1 ,3 ,5 ,6,12
+        # while(original in nums):
+        #     original *= 2
+        # return original
         nums.sort()
-        # [5,3,6,1,12]      1 ,3 ,5 ,6,12
-        while(original in nums):
-            original *= 2
         
+        for n in nums:
+            if original == n:
+                original*=2
         return original
+        
             
                         
         
