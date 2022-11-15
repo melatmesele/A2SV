c73baa36a7c71c158e955218e7819ca3885c5ab3
melatmesele <94954601+melatmesele@users.noreply.github.com>
2022-11-14 18:50:01 -0800

    Time: 193 ms (58.79%), Space: 13.9 MB (87.16%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,4 +1,41 @@
 class Solution:
-	def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-		temp=sorted(nums)
-		return [temp.index(_) for _ in nums]
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        ans = [0]*len(nums)
+        new = sorted(nums)
+        for i in range(len(nums)):
+            ans[i] = new.index(nums[i])
+        return ans
+        
+        # nums = [ 8 ,1 ,2 , 2, 3]
+        # new = [ 1 ,2 ,2 ,3 ,8]
+#         ans=  [4 ,0 ,1 ,1 ,3]
+       
+    
+#     new=sorted(nums)
+#     n=len(nums)
+#     ans=[0]*n
+    
+#     for i in range(n):
+#         ans[i]=new.index(nums[i])
+#     return ans    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+#         brute force
+#         ans = []
+        
+#         for i in range(len(nums)):
+#             counts = 0
+#             for j in range(len(nums)):
+#                            if nums[i] > nums[j]:
+#                                 counts+=1
+#             ans.append(counts)
+#         return ans
