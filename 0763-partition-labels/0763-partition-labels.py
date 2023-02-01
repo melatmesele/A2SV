31a96ae0a149785e86fdf641f18950317ab1e859
melatmesele <94954601+melatmesele@users.noreply.github.com>
2023-02-01 18:49:48 +0300

    Update 0763-partition-labels.py

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,16 +1,15 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         
-        d = {c:i for i, c in enumerate(s)}
-
-        l = 0
-        r = 0
-        ans = []
-        for i, c in enumerate(s):  
-            r = max(r, d[c])
-            if i == r:
-                ans += [r - l + 1]
-                l = r + 1
-
-        return ans
+        result = []
+        dictionary = {value: index  for index , value in enumerate(s)}
+        
+        start = 0
+        end = 0 
+        for index2 , value2 in enumerate(s):
+            end = max(end , dictionary[value2])
+            if index2 == end:
+                result.append(end - start + 1)
+                start = end + 1
+        return result
         
