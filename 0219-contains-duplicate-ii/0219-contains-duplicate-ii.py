3fb0a44db60a9f1172acf6454e4aacbfbf0d06a0
melatmesele <94954601+melatmesele@users.noreply.github.com>
2023-02-16 03:14:38 +0300

    Time: 661 ms (41.76%), Space: 25.4 MB (88.32%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,30 +1,39 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        dic = {}
         left = 0
-        right = 0
-        if len(nums) == 1:
-            return False
-        while right <= k and right < len(nums):
-            if nums[right] in dic:
-                return True
-            dic[nums[right]] =1
-            right += 1
-#         if len(dic)== k:
-#             return True
-            
-            
-            
-            
+        dic ={}
         flag = False
-        while right < len(nums):
-            del dic[nums[left]]
+        for right in range(len(nums)):
+            if right - left > k:
+                del dic[nums[left]]
+                left += 1
             if nums[right] in dic:
                 return True
             else:
-            
                 dic[nums[right]] = 1
-            left += 1
-            right += 1
         return flag
+#         dic = {}
+#         left = 0
+#         right = 0
+#         if len(nums) == 1:
+#             return False
+#         while right <= k and right < len(nums):
+#             if nums[right] in dic:
+#                 return True
+#             dic[nums[right]] =1
+#             right += 1
+# #         if len(dic)== k:
+# #             return True
+          
+#         flag = False
+#         while right < len(nums):
+#             del dic[nums[left]]
+#             if nums[right] in dic:
+#                 return True
+#             else:
+            
+#                 dic[nums[right]] = 1
+#             left += 1
+#             right += 1
+#         return flag
                 
