e00bb8819534a8a812eb411013b880363dfc76ce
melatmesele <94954601+melatmesele@users.noreply.github.com>
2023-02-17 01:20:04 +0300

    Time: 351 ms (38.70%), Space: 15.5 MB (41.94%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -4,21 +4,20 @@ class Solution:
         right = 0
         result = []
         diff = []
-        while right < k:
-            result.append(arr[right])
-            diff.append(abs(arr[right] - x))
-            right += 1
+        
         while right < len(arr):
-            difference = abs(arr[right] - x)
-            if difference < diff[left] or  result[0] == arr[right]:
-                diff.append(difference)
-                result.remove(result[0])
+            if right < k:
                 result.append(arr[right])
-                left += 1
-            # elif difference == diff[left] and result[0] > arr[right]:
-                
-            else:
-                break
+                diff.append(abs(arr[right] - x))
+            else:    
+                difference = abs(arr[right] - x)
+                if difference < diff[left] or  result[0] == arr[right]:
+                    diff.append(difference)
+                    result.remove(result[0])
+                    result.append(arr[right])
+                    left += 1
+                else:
+                    break
             right += 1
         return result
             
