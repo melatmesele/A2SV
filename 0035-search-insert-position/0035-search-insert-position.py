9fe363839eb89b95003fb477927c194e837ae92b
melatmesele <94954601+melatmesele@users.noreply.github.com>
2023-04-28 19:07:59 +0300

    Time: 65 ms (6.27%), Space: 17.2 MB (26.66%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,36 +1,45 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums)-1
-        result = inf
-        while(left <= right and right >=0):
+#         left = 0
+#         right = len(nums)-1
+#         result = inf
+#         while(left <= right and right >=0):
             
-            mid = left + (right - left)//2
+#             mid = left + (right - left)//2
+            
+#             if nums[mid] == target:
+#                 return mid
+#             elif nums[mid] < target:
+#                 left = mid + 1
+#                 result = mid + 1
+#             else:
+#                 right = mid -1
+#                 result = mid
+#         return result
+            
+        
+        
+        
+        
+        
+        left= 0
+        right = len(nums) - 1
+        result = 0 
+        
+        while(left <= right and right >= 0):
+            
+            mid = left +  (right- left)// 2
             
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
-                left = mid + 1
                 result = mid + 1
+                left = mid + 1
             else:
-                right = mid -1
-                result = mid
+                result = mid 
+                right = mid - 1
         return result
-            
-#             if left == len(nums)-1 and nums[left]< target:
-#                 return left + 1
-#             elif left == len(nums)-1 and nums[left] > target:
-#                 return left 
-#             elif  right  ==0 and nums[left] > target:
-#                 return left 
-#             elif right  ==0  and nums[left] > target:
-#                 return left 
-#             elif nums[mid] == target:
-#                 return mid
-#             elif nums[mid] < target:
-#                 left = mid +1
-#             elif nums[mid] > target:
-#                 right = mid - 1
-            
+                
         
+
                 
