f2dae194c3291b217ee765fb336ee77b4e286cbc
melatmesele <94954601+melatmesele@users.noreply.github.com>
2023-04-28 19:33:03 +0300

    Time: 52 ms (26.99%), Space: 16.2 MB (5.62%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -8,13 +8,15 @@ class Solution:
         result = inf
         while(left <= right):
             mid = left + (right -left)//2
-                
-            if mid*mid <=  x:
+            if mid*mid == x:
+                return mid
+            elif mid*mid <  x:
                 result = mid
                 left = mid + 1
         
-            elif mid*mid > x:
-                right = mid -1
+            else:
+                right = mid - 1
+                result = mid - 1
         return result
                 
                 
