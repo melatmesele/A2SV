98a1deb85b9b1da6f88b2bd92b164f9e2e8d2b54
melatmesele <94954601+melatmesele@users.noreply.github.com>
2023-06-02 12:11:43 +0300

    Time: 61 ms (46.08%), Space: 17.8 MB (29.28%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -4,16 +4,20 @@ class Solution:
         # for i in s:
         #     dic[i] += 1
         dic = Counter(s)
-        ans = []
-        for item in dic:
-            ans.append((-(dic[item]) , item))
-        ans.sort()
-        result = ''
-        for i in ans:
+        s = list(set(s))
+        s.sort(key= lambda x: dic[x] * -1)
+        output = [x * dic[x] for x in s]
+        return "".join(output)
+#         ans = []
+#         for item in dic:
+#             ans.append((-(dic[item]) , item))
+#         ans.sort()
+#         result = ''
+#         for i in ans:
             
-            result += i[1]* -(i[0])
-        return result
-        
+#             result += i[1]* -(i[0])
+        # return result
+                
         
             
         
