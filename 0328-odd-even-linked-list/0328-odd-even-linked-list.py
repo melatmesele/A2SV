9d8932c1f16375c531503b38c98d81feafd4ec2f
melatmesele <94954601+melatmesele@users.noreply.github.com>
2023-06-17 12:25:36 +0300

    Time: 59 ms (54.44%), Space: 19 MB (15.40%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -5,18 +5,44 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        evenDummy = ListNode(0,None)
+        even  = evenDummy
+        oddDummy = ListNode(0,None)
+        odd = oddDummy
         if head == None:
-            return 
-        odd = head
-        even = odd.next
-        even_head = even
-        
-        while even != None and even.next:
-            odd.next = even.next
-            odd = odd.next
+            return
+        curr = head
+        counter = 1
+        while(curr):
+            if counter % 2 != 0:
+                odd.next = curr
+                odd = curr
+                curr = curr.next
+            else:
+                even.next = curr
+                even = curr
+                curr = curr.next
+            counter += 1
+        odd.next = evenDummy.next
+        even.next = None
+        return oddDummy.next
+                
+                
+                
             
-            even.next = odd.next
-            even = even.next
-        odd.next = even_head
-        return head
+        
+#         if head == None:
+#             return 
+#         odd = head
+#         even = odd.next
+#         even_head = even
+        
+#         while even != None and even.next:
+#             odd.next = even.next
+#             odd = odd.next
+            
+#             even.next = odd.next
+#             even = even.next
+#         odd.next = even_head
+#         return head
         
